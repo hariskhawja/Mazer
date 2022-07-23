ae78c1d15283967f65ac11a429c44732be229c82
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2022-07-23 10:59:50 -0400

    completed mazer

--- main.py
+++ main.py
@@ -9,36 +9,64 @@ screen = pygame.display.set_mode((1200, 600), pygame.RESIZABLE)
 FPS = 60
 fpsClock = pygame.time.Clock()
 
-player = playerControl.Player('white', [10, 10], 20, 4)
+player = playerControl.Player('white', [10, 10], 20, 3)
+
+cheats = False
+
+gameOver = False
 
 quitVar = True
 
 while quitVar:
     screen.fill('black')
 
-    player.playerDraw(screen)
-    
-    for obstacle in obstacleInitialise.obstaclesList:
-        obstacle.obstacleDraw(screen)
-        if player.playerRect.colliderect(obstacle.obstacleRect):
-            player.playerDelay = 25
-            player.pos = [10, 10]
+    w, h = pygame.display.get_surface().get_size()
 
-    keys = pygame.key.get_pressed()
-
-    if player.playerDelay <= 0:
-        if keys[pygame.K_UP]: player.moveNorth()
-
-        if keys[pygame.K_DOWN]: player.moveSouth()
-
-        if keys[pygame.K_RIGHT]: player.moveEast()
-
-        if keys[pygame.K_LEFT]: player.moveWest()
+    if gameOver:
+        pygame.draw.rect(screen, "gold", (w / 2 - 150, h / 2 + 100, 300, 10))
+        pygame.draw.rect(screen, "gold", (w / 2 - 150, h / 2 - 100, 10, 200))
+        pygame.draw.rect(screen, "gold", (w / 2 + 140, h / 2 - 100, 10, 200))
+        pygame.draw.rect(screen, "gold", (w / 2 - 140, h / 2 - 100, 50, 10))
+        pygame.draw.rect(screen, "gold", (w / 2 + 90, h / 2 - 100, 50, 10))
+        pygame.draw.rect(screen, "gold", (w / 2 - 90, h / 2 - 100, 10, 75))
+        pygame.draw.rect(screen, "gold", (w / 2 + 80, h / 2 - 100, 10, 75))
+        pygame.draw.rect(screen, "gold", (w / 2 - 90, h / 2 - 25, 50, 10))
+        pygame.draw.rect(screen, "gold", (w / 2 + 40, h / 2 - 25, 50, 10))
+        pygame.draw.rect(screen, "gold", (w / 2 - 40, h / 2 - 150, 10, 135))
+        pygame.draw.rect(screen, "gold", (w / 2 + 30, h / 2 - 150, 10, 135))
+        pygame.draw.rect(screen, "gold", (w / 2 - 40, h / 2 - 150, 75, 10))
 
     else:
-        player.playerDelay -= 1
+        player.playerDraw(screen)
+    
+        for obstacle in obstacleInitialise.obstaclesList:
+            obstacle.obstacleDraw(screen)
+            if player.playerRect.colliderect(obstacle.obstacleRect):
+                if obstacle.colour == "green": gameOver = True
+            
+                if not cheats:
+                    player.playerDelay = 25
+                    player.pos = [10, 10]
+
+        keys = pygame.key.get_pressed()
+
+        if player.playerDelay <= 0:
+            if keys[pygame.K_UP]: player.moveNorth()
+
+            if keys[pygame.K_DOWN]: player.moveSouth()
+
+            if keys[pygame.K_RIGHT]: player.moveEast()
+
+            if keys[pygame.K_LEFT]: player.moveWest()
+
+        else:
+            player.playerDelay -= 1
 
     for event in pygame.event.get():
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c: cheats = not cheats
+
         if event.type == pygame.QUIT:
             quitVar = False
 
